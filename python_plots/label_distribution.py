b2af2bce12a348bac5dd3f6da80a50836912f07d
Robert Walecki <rw2614@imperial.ac.uk>
2015-07-12 00:32:21 +0100

    add label plot

---
+++ python_plots/label_distribution.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def label_distribution(data,
+               m_name='',
+               s_name='',
+               title='',
+               ylabel = '',
+               xlabel = '',
+               xname = [],
+               ylim = [],
+               legend_size = 12,
+               legend_loc = 1,
+               path='/tmp/test.pdf'
+               ):
+
+    fig, ax = plt.subplots()
+    classes = np.unique(dat)
+
+    c_map = cm.Spectral_r(np.linspace(0, 1, len(classes)*2))
+    for i in classes[::-1]:
+        tmp = np.sum(dat<=i,1)
+        plt.bar(range(len(tmp)),tmp,color=c_map[i])
+
+
+    ax.set_xticklabels( xname )
+    ax.set_xticks( np.arange(len(xname))+0.4 )
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend(classes[::-1],fontsize=legend_size,loc = legend_loc)
+    plt.savefig(path)
+
+
+
+
+if __name__ == "__main__":
+
+    dat = np.random.randint(0,7,(10,10))
+    label_distribution(
+        dat,
+        xname=[1,2,3,4,5,666,7,8,999,10],
+        xlabel='AUs',
+        ylabel='# - active',
+    )
+
+
+
