7bad9633bac996bdbacda6ca9e0acb720174540f
Robert Walecki <rw2614@imperial.ac.uk>
2015-08-10 11:37:05 +0200

    add graph plot

--- python_scripts/QuickPlots.py
+++ python_scripts/QuickPlots.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.gridspec as gridspec
+import daft
+
 
 def ScatterMatrix(
                     data,
@@ -11,9 +14,12 @@ def ScatterMatrix(
 
     n = len(data)
 
+    gs = gridspec.GridSpec(n+1,n+1)
+
+
     for i in range(n):
         for j in range(n):
-            fig = plt.subplot2grid((n,n),(i, j))
+            fig = plt.subplot(gs[i, j+1])
             #fig.axis('off')
             fig.axes.get_xaxis().set_visible(False)
             fig.axes.get_yaxis().set_visible(False)
@@ -31,6 +37,8 @@ def ScatterMatrix(
                         normed=1,
                         facecolor='green',
                         )
+                ratio_default=(fig.get_xlim()[1]-fig.get_xlim()[0])/(fig.get_ylim()[1]-fig.get_ylim()[0])
+                fig.set_aspect(ratio_default)
             #########################################################
 
             if i < j:
@@ -43,8 +51,6 @@ def ScatterMatrix(
                 txt = str(np.round(cor*100)/100)
                 plt.imshow(tmp,vmin=-1, vmax=1)
                 fig.text(1,1,txt,ha="center", va="center")
-                fig.set_aspect('equal', 'datalim')
-                plt.axis('scaled')
             #########################################################
 
             if i > j:
@@ -53,10 +59,57 @@ def ScatterMatrix(
             # shows the distribution
             #########################################################
                 plt.scatter(x,y)
+                ratio_default=(fig.get_xlim()[1]-fig.get_xlim()[0])/(fig.get_ylim()[1]-fig.get_ylim()[0])
+                fig.set_aspect(ratio_default)
             #########################################################
+
             plt.suptitle(title)
 
+
+    plt.tight_layout()
     if save:
         plt.savefig(save)
     else:
         plt.show()
+
+def DrawGraph(Np,Ep=None,name=None):
+    pgm = daft.PGM([8, 8], origin=[-4, -4],directed=False,node_unit=1, grid_unit=1)
+    N = Np.shape[0]
+    alpha = np.linspace(0,2*np.pi,N+1)[:-1]
+
+    for i,a in enumerate(alpha):
+
+        x = np.cos(a+np.pi/2)*3
+        y = np.sin(a+np.pi/2)*3
+
+        if name:
+            label = name[i]
+        else:
+            label = str(i)
+
+        pgm.add_node(daft.Node(i, label, x, y,observed=False))
+
+    if Ep:
+        for e1,e2 in Ep:
+            pgm.add_edge(e1, e2)
+
+    pgm.render()
+    pgm.figure.savefig("/tmp/tmp.pdf")
+
+
+if __name__ == "__main__":
+
+    import itertools
+    N = 6
+    n = np.random.rand(N,6)
+    name = [
+        '$AU_1$',
+        '$AU_2$',
+        '$AU_4$',
+        '$AU_6$',
+        '$AU_{12}$',
+        '$AU_{20}$',
+    ]
+    e = []
+    for i in itertools.combinations(range(N),2):e.append(i)
+    DrawGraph(n,e,name)
