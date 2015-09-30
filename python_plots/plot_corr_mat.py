4cb463d19542388030aacb7c0f636c88176b9a89
Robert Walecki <rw2614@imperial.ac.uk>
2015-09-30 13:25:46 -0400

    ...

--- python_plots/plot_corr_mat.py
+++ python_plots/plot_corr_mat.py
@@ -1,16 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+
 def plot_corr_mat(
-    data,
+    corr_mat,
     xname = None,
     xlabel= '',
-    path = '/tmp/test.pdf'
+    trashold = 0,
+    path = '/tmp/test.pdf',
 ):
-    corr = np.corrcoef(data)
     fig, ax = plt.subplots()
-    plt.imshow(corr,interpolation='none',cmap=cm.Spectral_r)
-    N = corr.shape[1]
+    corr_mat[np.abs(corr_mat)<trashold]=0
+    v_max = np.max(np.abs(corr_mat))
+    plt.imshow(corr_mat.T,
+               interpolation='nearest',
+               cmap=cm.seismic,
+               vmin=-v_max, vmax=v_max
+               )
+    N = corr_mat.shape[1]
     if xname == None:
         xname = range(N)
 
@@ -22,8 +29,29 @@ def plot_corr_mat(
     ax.set_yticks( range(N) )
     ax.set_ylabel(xlabel)
 
-    for x in range(corr.shape[0]):
-        for y in range(corr.shape[1]):
-            plt.text(x-0.33, y+0.1, np.int8(corr[x,y]*100)/100.,size=10)
+    min_val, max_val = 0, corr_mat.shape[0]
+    ind_array = np.arange(min_val, max_val, 1.0)
+    x, y = np.meshgrid(ind_array, ind_array)
 
+    for i, (x_val, y_val) in enumerate(zip(x.flatten(), y.flatten())):
+        val = corr_mat[x_val,y_val]
+        if val==0:continue
+        c = np.int8(val*100)/100.
+        ax.text(x_val, y_val, c, va='center', ha='center',size=10)
     plt.savefig(path)
+
+
+if __name__ == "__main__":
+    C = (np.random.rand(10,10)*2)-1
+    prune_factor = 0.7
+
+    for x in range(C.shape[0]):
+        for y in range(C.shape[1]):
+            if x==y:continue
+            if np.random.rand()>prune_factor:continue
+            C[x,y]=0
+
+    plot_corr_mat(
+        corr_mat=C,
+        xname = [1,2,3,4,5,6,7,8,-9,100]
+    )
